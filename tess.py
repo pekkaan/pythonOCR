67152953d3b3263138e01940c8324454ddde9634
tokubanq <63857981+tokubanq@users.noreply.github.com>
2020-06-23 11:08:41 +0300

    Update tess.py

--- tess.py
+++ tess.py
@@ -18,13 +18,15 @@ def add_coords():
 mouse.on_click(add_coords)
 keyboard.wait("ctrl+shift")
 
-img = ImageGrab.grab(bbox=(coords[0][0], coords[0][1], coords[1][0], coords[1][1]))  # Unfortunately ImageGrab seems to create blurry images
+img = ImageGrab.grab(bbox=(coords[0][0], coords[0][1], coords[1][0], coords[1][1]))
 img.save("temp_pic.png", "png")
 """
+pdf_file = "1055_RK-B101.pdf"
 imgs_path = os.path.join(os.getcwd(), "images")
-print("converting pdf to images...")
-imgs = pdf2image.convert_from_path("1055_RK-B101.pdf", output_folder=imgs_path, poppler_path="C:\\Users\\tommi\\miniconda3\\pkgs\\poppler-0.89.0-h0cd1227_0\\Library\\bin")
-print("converted, trying to find text...")
+bin_path = os.path.join(os.getcwd(), "bin")
+print("start")
+imgs = pdf2image.convert_from_path(pdf_file, fmt="jpeg", output_folder=imgs_path, poppler_path=bin_path)
+print("pdf converted")
 for img in imgs:
     img_txt = pytesseract.image_to_string((img), lang="fin")
     #img_box = pytesseract.image_to_boxes((img), lang="fin")  # massively impacts performance (from 14s to 26s)
