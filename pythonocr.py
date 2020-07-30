af06a67f1028214063be4c3f78ad7866324791d5
Juho Koponen <juho.koponen@q-factory.fi>
2020-07-30 16:43:12 +0300

    Documentation fixes (grammar, consistency).

--- pythonocr.py
+++ pythonocr.py
@@ -35,8 +35,8 @@ Python OCR main functions can be found at the end of this file, internal functio
 
 Internal functions:
 
-    _get_word_coordinates_from_data(): Retrieves the data of found instances of a word and their 
-                                       coordinates from image data.
+    _get_word_coordinate_from_data(): Retrieves the data of found instances of a word and their coordinates
+                                      from image data.
 
     _get_poppler_path(): Returns directory path of poppler files.
 
@@ -56,7 +56,7 @@ def _get_word_coordinates_from_data(word, image_data, page_number=0):
     :param image_data: Given image data, such as data retrieved by pytesseract. Required.
                        Must be a dictionary of lists.
     :param page_number: Page number to be included with the returned data. Optional. If 0 (zero) or less,
-                        page number is not included in the returned data. By default 0.
+                        page number is not included in the returned data. By default, 0.
     :return: Found instances of the word and their coordinates in image.
              A list of tuples, each element consisting of: (found text, left coordinates (X), top coordinates (Y),
              text width, text height, page number (Optional)).
@@ -126,9 +126,8 @@ def click_word(word, save_screenshot_as=""):
     Searches for a specified word on screen and clicks the word's location. Takes a screenshot using pyautogui
     and recognizes text in it with pytesseract. Optionally, can save screenshot as a specified .png file.
 
-    If able to find a single instance of the word, retrieves it's
-    coordinates and clicks the location with mouse, using pyautogui. Unable to click the word's location if
-    multiple instances of the word are found on screen!
+    If able to find a single instance of the word, retrieves it's coordinates and clicks the location with mouse,
+    using pyautogui. Unable to click the word's location if multiple instances of the word are found on screen!
 
     :param word: The specified word. Required. Upper and lowercase sensitive!
     :type word: str
@@ -172,8 +171,8 @@ def find_words(word, file_path, output_path="./"):
     :type word: str
     :param file_path: Image or PDF file path. Required. Can be absolute or relative to the current project directory.
     :type file_path: str
-    :param output_path: Output directory for image files converted from the PDF file.
-                        By default current project directory.
+    :param output_path: Output directory for image files converted from the PDF file. Not required if processing
+                        image files. By default, current project directory.
     :type output_path: str
     :return: A list of found instances of the specified word.
              A list of tuples, each element consisting of: (found text, page number).
@@ -209,7 +208,7 @@ def find_coordinates(word, file_path, output_path="./"):
     :param file_path: Image or PDF file path. Required. Can be absolute or relative to the current project directory.
     :type file_path: str
     :param output_path: Output directory for image files converted from the PDF file.
-                        By default current project directory.
+                        By default, current project directory.
     :type output_path: str
     :return: Found instances of the word and their coordinates in image.
              A list of tuples, each element consisting of: (found text, left coordinates (X), top coordinates (Y),
@@ -239,7 +238,7 @@ def verify_word(word, image_path):
     :type word: str
     :param image_path: Image file path. Required. Can be absolute or relative to the current project directory.
     :type image_path: str
-    :return: Returns True if found at least one instance of specified word in image, False if none.
+    :return: Returns True if found at least one instance of the specified word in image, False if none.
              Returns None if image is not .jpg, .jpeg nor .png file.
     :rtype: bool
     """
